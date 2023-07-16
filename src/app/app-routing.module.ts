9be16523e6068e8bd56129b3b65c71f26e16e367
Juanjo <juanjose.ortizgarcia@outlook.com>
2023-07-16 07:20:34 +0200

    Add error404Page and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'error404', component: Error404PageComponent },
+  { path: '**', redirectTo: 'error404' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Error404PageComponent
   ],
   imports: [
     BrowserModule,
