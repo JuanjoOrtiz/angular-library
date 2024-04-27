74a7ff88e87572b5da5b189beccb2e9591067d79
Juanjo <juanjose.ortizgarcia@outlook.com>
2024-04-27 17:42:52 +0200

    add loans component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,13 +5,15 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardLayoutComponent } from './layout/dashboard-layout.component';
 import { BooksComponent } from './pages/books/books.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoansComponent } from './pages/loans/loans.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardLayoutComponent,
-    BooksComponent
+    BooksComponent,
+    LoansComponent
   ],
   imports: [
     CommonModule,
